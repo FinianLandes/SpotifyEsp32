ca7b16e22e66103f296069e5300429d832e17bab
Chaerne <finianlandes@gmail.com>
2024-02-20 10:59:16 +0100

    Updated readme and updated the examples

---
+++ examples/Login without refresh Token/login_without_refresh_token.ino
@@ -0,0 +1,45 @@
+/*
+    An example of how to authenticate with Spotify without using a refresh token.
+
+    This example is useful to get the refresh token for the first time. It can also be used to authenticate every time without using the refresh token.
+
+    20.02.2024
+    Created by: Finian Landes
+
+    Documentation: https://github.com/FinianLandes/Spotify_Esp32
+*/
+// Include the required libraries
+#include <Arduino.h>
+#include <WiFi.h>
+#include "SpotifyESP32.h"
+
+const char* SSID = "YOUR WIFI SSID";
+const char* PASSWORD = "YOUR WIFI PASSWORD";
+const char* CLIENT_ID = "YOUR CLIENT ID FROM THE SPOTIFY DASHBOARD";
+const char* CLIENT_SECRET = "YOUR CLIENT SECRET FROM THE SPOTIFY DASHBOARD";
+
+Spotify sp(CLIENT_ID, CLIENT_SECRET);
+
+void setup() {
+    Serial.begin(115200);
+    connect_to_wifi();
+    
+    sp.begin();
+    while(!sp.is_auth()){
+        sp.handle_client();
+    }
+    Serial.println("Authenticated");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
+void connect_to_wifi(){
+    WiFi.begin(SSID, PASSWORD);
+    Serial.print("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.printf("\nConnected to WiFi\n");
+}
