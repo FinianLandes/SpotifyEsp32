877c799bd5aa6e7fbe318a1890770a842a1bc2e3
Chaerne <99126886+FinianLandes@users.noreply.github.com>
2024-03-10 17:56:38 +0100

    Delete src/config.h

--- src/config.h
+++
@@ -1,6 +0,0 @@
-const char* ssid = "YOUR WIFI SSID";
-const char* password = "YOUR WIFI PASSWORD";
-const char* client_id = "YOUR CLIENT ID FROM THE SPOTIFY DASHBOARD";
-const char* client_secret = "YOUR CLIENT SECRET FROM THE SPOTIFY DASHBOARD";
-const char* refresh_token = "YOUR REFRESH TOKEN IF YOU ALREADY HAVE ONE";
-
